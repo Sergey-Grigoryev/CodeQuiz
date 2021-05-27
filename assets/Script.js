b75fead81c09ec5e1aedfadaf5a38dd814c2903b
ruserge88 <ruserge88@gmail.com>
2021-05-27 15:34:51 -0700

    added High Score, and final score feedback

--- assets/Script.js
+++ assets/Script.js
@@ -18,48 +18,18 @@ var question1 = [
     {choice1:"var", choice2:"let", choice3:"const", choice4:"thing"}, 
     {correct:"thing"}];
 var question2 = [
-    {question:"Second question"}, 
-    {choice1:"option 1", choice2:"option 2", choice3:"option 3", choice4:"option 4"}, 
-    {correct:"option 2"}];
+    {question:"Which of the following links to a class in an HTML doc?"}, 
+    {choice1:".classname", choice2:"#classname", choice3:"%classname", choice4:"All of the above"}, 
+    {correct:".classname"}];
 var question3 = [
-    {question:"Third question"}, 
-    {choice1:"option 1", choice2:"option 2", choice3:"option 3", choice4:"option 4"}, 
-    {correct:"option 3"}];
+    {question:"What does the clearInterval() do in your setInterval(function(){}) ?"}, 
+    {choice1:"Clears everything on the screen", choice2:"Clears cache", choice3:"Stops setInterval from running forever", choice4:"Does nothing"}, 
+    {correct:"Stops setInterval from running forever"}];
 var question4 = [
-    {question:"Fourth question"}, 
-    {choice1:"option 1", choice2:"option 2", choice3:"option 3", choice4:"option 4"}, 
-    {correct:"option 4"}];
+    {question:"Who of the 6 Friends had the first line in the first episode season 1"}, 
+    {choice1:"Monica", choice2:"Ross", choice3:"Joe", choice4:"Phoebe"}, 
+    {correct:"Monica"}];
 var finalScore = {};
-
-    // 60 sec timer
-let timeRemaining = 59;
-function duduct10() {
-    timeRemaining -= 10;
-};
-function timer () {    
-    var timeInterval = setInterval(function() {
-        // stop timer after last question
-        if (currentQuestion === finalScore) {
-            clearInterval(timeInterval);
-        };
-        secondsLeft.textContent = timeRemaining;
-        timeRemaining--;
-        if (timeRemaining < 0) {
-            clearInterval(timeInterval);
-            alert("Your time ran out!");
-            endQuiz();
-        };
-    }, 1000);
-};
-
-    // End of Quiz
-function endQuiz() {
-    currentQuestion = finalScore;
-    questions.textContent = ("Good Job! You completed the quiz.")
-    multiChoice.style.display = "none";
-    document.querySelector(".feedback").style.display = "none";
-    initialsForm.style.display = "flex";
-};
 
     //start of quiz
 startQuizBtn.addEventListener("click", function startQuiz(event) {
@@ -90,7 +60,6 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
     multiChoice.appendChild(createOptionBtn(currentQuestion[1].choice2));
     multiChoice.appendChild(createOptionBtn(currentQuestion[1].choice3));
     multiChoice.appendChild(createOptionBtn(currentQuestion[1].choice4));
-    console.log (currentQuestion[2].correct);
 
     function secondQuestion() {
 
@@ -101,10 +70,10 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
         // show the answer options in buttons
         multiChoice.style.display = "flex";
 
-        document.querySelector("#choice1").textContent = "Test2.1";
-        document.querySelector("#choice2").textContent = "Test2.2";
-        document.querySelector("#choice3").textContent = "Test2.3";
-        document.querySelector("#choice4").textContent = "Test2.4";
+        document.querySelector("#choice1").textContent = currentQuestion[1].choice1;
+        document.querySelector("#choice2").textContent = currentQuestion[1].choice2;
+        document.querySelector("#choice3").textContent = currentQuestion[1].choice3;
+        document.querySelector("#choice4").textContent = currentQuestion[1].choice4;
         console.log (currentQuestion[2].correct);
     };
 
@@ -117,10 +86,10 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
         // show the answer options in buttons
         multiChoice.style.display = "flex";
 
-        document.querySelector("#choice1").textContent = "Test3.1";
-        document.querySelector("#choice2").textContent = "Test3.2";
-        document.querySelector("#choice3").textContent = "Test3.3";
-        document.querySelector("#choice4").textContent = "Test3.4";
+        document.querySelector("#choice1").textContent = currentQuestion[1].choice1;
+        document.querySelector("#choice2").textContent = currentQuestion[1].choice2;
+        document.querySelector("#choice3").textContent = currentQuestion[1].choice3;
+        document.querySelector("#choice4").textContent = currentQuestion[1].choice4;
         console.log (currentQuestion[2].correct);
     };
 
@@ -133,14 +102,14 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
         // show the answer options in buttons
         multiChoice.style.display = "flex";
 
-        document.querySelector("#choice1").textContent = "Test4.1";
-        document.querySelector("#choice2").textContent = "Test4.2";
-        document.querySelector("#choice3").textContent = "Test4.3";
-        document.querySelector("#choice4").textContent = "Test4.4";
+        document.querySelector("#choice1").textContent = currentQuestion[1].choice1;
+        document.querySelector("#choice2").textContent = currentQuestion[1].choice2;
+        document.querySelector("#choice3").textContent = currentQuestion[1].choice3;
+        document.querySelector("#choice4").textContent = currentQuestion[1].choice4;
         console.log (currentQuestion[2].correct);
     };
 
-        // score counter
+        // score counter + feedback
     var choice1 = document.querySelector("#choice1");
     var choice2 = document.querySelector("#choice2");
     var choice3 = document.querySelector("#choice3");
@@ -157,18 +126,34 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
             document.querySelector(".feedback").style.display = "flex";
             document.querySelector(".feedback").textContent = "Wrong! -10 sec";
         };
+        setTimeout(function(){
+            document.querySelector(".feedback").style.display = "none";
+        }, 1500);
     };
 
+        // End of quiz initials entry
     initialsFormBtn.addEventListener("click", function saveScore() {
+        event.preventDefault();
         let savedScore = JSON.parse(localStorage.getItem("Score"));
-        if (score > savedScore) {
+        if (score >= savedScore) {
             localStorage.setItem("Tester", JSON.stringify(initialsFormText.value));
             localStorage.setItem("Score", JSON.stringify(score));
-        } 
+        };
     });
 
+        // End of Quiz
+        function endQuiz() {
+            currentQuestion = finalScore;
+            questions.textContent = ("Good Job! You completed the quiz!");
+            multiChoice.style.display = "none";
+            initialsForm.style.display = "flex";
+            document.querySelector(".aboutQuiz").style.display = "flex";
+            document.querySelector(".aboutQuizP").style.display = "none";
+            document.querySelector(".aboutQuizH3").textContent = "You scored: " + score;
+        };
+
         // Answer selection listeners 
-    choice1.addEventListener("click", function scoreCalcChoice1() {
+    choice1.addEventListener("click", function () {
         scoreCalc(currentQuestion[1].choice1);
         if (currentQuestion === question1) {
             secondQuestion();
@@ -181,7 +166,7 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
         }
     } );
 
-    choice2.addEventListener("click", function scoreCalcChoice2() {
+    choice2.addEventListener("click", function () {
         scoreCalc(currentQuestion[1].choice2);
         if (currentQuestion === question1) {
             secondQuestion();
@@ -193,7 +178,8 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
             endQuiz();
         }
     } );
-    choice3.addEventListener("click", function scoreCalcChoice3() {
+    choice3.addEventListener("click", function (event) {
+        console.log(event.target)
         scoreCalc(currentQuestion[1].choice3);
         if (currentQuestion === question1) {
             secondQuestion();
@@ -205,7 +191,7 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
             endQuiz();
         }
     } );
-    choice4.addEventListener("click", function scoreCalcChoice4() {
+    choice4.addEventListener("click", function () {
         scoreCalc(currentQuestion[1].choice4);
         if (currentQuestion === question1) {
             secondQuestion();
@@ -217,44 +203,47 @@ startQuizBtn.addEventListener("click", function startQuiz(event) {
             endQuiz();
         }
     } );
+
+        // 60 sec timer
+let timeRemaining = 59;
+function duduct10() {
+    timeRemaining -= 10;
+};
+    function timer () {    
+        var timeInterval = setInterval(function() {
+                // stop timer after last question
+            if (currentQuestion === finalScore) {
+                clearInterval(timeInterval);
+            };
+            secondsLeft.textContent = timeRemaining;
+            timeRemaining--;
+            if (timeRemaining < 0) {
+                clearInterval(timeInterval);
+                alert("Your time ran out!");
+                endQuiz();
+            };
+        }, 1000);
+    };
 });
 
 let highTester = JSON.parse(localStorage.getItem("Tester"));
 let highScore = JSON.parse(localStorage.getItem("Score"));
-console.log(highScore, highTester);
+let viewHighScore = false;
+console.log(highTester);
 
 scoresLink.addEventListener("click", function showHighScore(event) {
     event.preventDefault();
-    pastScores.style.display = "flex";
-
-    document.querySelector(".pastScoresHighest").textContent =  highTester + " scored: " + highScore;
+    if (viewHighScore === false) {
+        viewHighScore = true;
+        pastScores.style.display = "flex";
+        if (highTester) {
+            pastScores.style.display = "flex";
+            document.querySelector(".pastScoresHighest").textContent =  highTester + " scored: " + highScore;
+        } else {
+            document.querySelector(".pastScoresHighest").textContent = "No high score yet!";
+        };
+    } else {
+        viewHighScore = false;
+        pastScores.style.display = "none";
+    };
 });
-
-
-  
-
-
-
-// choice1.addEventListener("click", function scoreCalc(choice1) {
-//     if (question1[1].choice1 === question1[2].correct) {
-//         console.log(score);
-//         score++;
-//         console.log(score);
-//     }
-// });
-// choice2.addEventListener("click", function scoreCalc(choice2) {
-//     if (question1[1].choice2 === question1[2].correct) {
-//         score++;
-//     };        
-// });        
-// choice3.addEventListener("click", function scoreCalc(choice3) {
-//     if (question1[1].choice3 === question1[2].correct) {
-//         score++;
-//     };        
-// });
-// choice4.addEventListener("click", function scoreCalc(choice4) {
-//     if (question1[1].choice4 === question1[2].correct) {
-//         score++;
-//     };        
-// });
-
